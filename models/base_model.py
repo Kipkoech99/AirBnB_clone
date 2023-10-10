a84c253fcd97c2270ba4cd80f245c9691e0e96df
Elly <122857130+EljonesA@users.noreply.github.com>
2023-10-10 14:21:50 +0000

    Update BaseModel to include ability to create instance from dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,20 @@ class BaseModel:
         save: updates the updated_at instance
         to_dict: returns dictionary representation of the object
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Class constructor'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:  # initialize attributes from kwargs
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''Returns string repr of the object'''
